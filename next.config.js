5dab2fd6ea498b6f7719d625b4d541a0f04a6685
vanphuagl <nguyen_van_phu@allgrow-labo.jp>
2023-11-16 07:58:08 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  distDir: "dist",
+  output: "export",
+  swcMinify: true,
+  trailingSlash: true,
+  experimental: {
+    scrollRestoration: true
+  },
+  images: {
+    loader: 'custom',
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
